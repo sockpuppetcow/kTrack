9fcb533898e716f1ad5764cc25aeb4ed175c184e
sockpuppetcow <sockpuppetcow@gmail.com>
2021-09-18 12:56:01 -0500

    Scaffold Identity into project for finer control over UI and other properties

---
+++ kTrack/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using kTrack.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(kTrack.Areas.Identity.IdentityHostingStartup))]
+namespace kTrack.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
