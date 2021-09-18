295315133409314878ccf9dc7edf6e85fff14812
sockpuppetcow <sockpuppetcow@gmail.com>
2021-09-18 14:12:38 -0500

    Create initial models for issue, label, project, and user profile.
    User profile will use data from ApplicationUser to provide relevant information without exposing secrets such as password hash, etc.

---
+++ kTrack/Models/Issue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace kTrack.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        public Project ProjectRef { get; set; }
+        public virtual UserProfile Author { get; set; }
+        public String Title { get; set; }
+        public String Body { get; set; }
+        public Label[] Labels { get; set; }
+    }
+}
---
+++ kTrack/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kTrack.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+    }
+}
