3c26907804cc3d7933410b9044684bb3fd2d5dd0
sockpuppetcow <sockpuppetcow@gmail.com>
2021-09-23 16:14:19 -0500

    Apply Foreign Key references to Issue model to link projects and users

--- kTrack/Models/Issue.cs
+++ kTrack/Models/Issue.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -9,7 +10,11 @@ namespace kTrack.Models
     public class Issue
     {
         public int Id { get; set; }
-        public Project ProjectRef { get; set; }
+        public int ProjectRefId { get; set; }
+        [ForeignKey("ProjectRefId")]
+        public virtual Project Project { get; set; }
+        public int AuthorId { get; set; }
+        [ForeignKey("AuthorId")]        
         public virtual UserProfile Author { get; set; }
         public String Title { get; set; }
         public String Body { get; set; }
