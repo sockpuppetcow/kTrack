ba412e2d76eebaa9a69ee297bd8f5e9d1efe8dc4
sockpuppetcow <sockpuppetcow@gmail.com>
2021-09-26 12:49:11 -0500

    Add basic Controller and Views for Issues

---
+++ kTrack/Controllers/IssuesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using kTrack.Data;
+using kTrack.Models;
+
+namespace kTrack.Controllers
+{
+    public class IssuesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IssuesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Issues
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Issue.Include(i => i.Author).Include(i => i.Project);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Issues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issue
+                .Include(i => i.Author)
+                .Include(i => i.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return View(issue);
+        }
+
+        // GET: Issues/Create
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["ProjectRefId"] = new SelectList(_context.Project, "Id", "Id");
+            return View();
+        }
+
+        // POST: Issues/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProjectRefId,AuthorId,Title,Body")] Issue issue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(issue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", issue.AuthorId);
+            ViewData["ProjectRefId"] = new SelectList(_context.Project, "Id", "Id", issue.ProjectRefId);
+            return View(issue);
+        }
+
+        // GET: Issues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issue.FindAsync(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", issue.AuthorId);
+            ViewData["ProjectRefId"] = new SelectList(_context.Project, "Id", "Id", issue.ProjectRefId);
+            return View(issue);
+        }
+
+        // POST: Issues/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProjectRefId,AuthorId,Title,Body")] Issue issue)
+        {
+            if (id != issue.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(issue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IssueExists(issue.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Users, "Id", "Id", issue.AuthorId);
+            ViewData["ProjectRefId"] = new SelectList(_context.Project, "Id", "Id", issue.ProjectRefId);
+            return View(issue);
+        }
+
+        // GET: Issues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var issue = await _context.Issue
+                .Include(i => i.Author)
+                .Include(i => i.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return View(issue);
+        }
+
+        // POST: Issues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var issue = await _context.Issue.FindAsync(id);
+            _context.Issue.Remove(issue);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IssueExists(int id)
+        {
+            return _context.Issue.Any(e => e.Id == id);
+        }
+    }
+}
