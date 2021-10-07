0e977e29fbc21272ec05f0221d01300de0d0e874
sockpuppetcow <sockpuppetcow@gmail.com>
2021-10-07 11:53:37 -0500

    Add Test controller for testing user authentication/authorization

---
+++ kTrack/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kTrack.Controllers
+{
+    [AllowAnonymous]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            string content = User.Identity.IsAuthenticated + "\n"
+                + User.Identity.Name + "\n"
+                + User.Identity.AuthenticationType;
+            return Content(content);
+        }
+    }
+}
