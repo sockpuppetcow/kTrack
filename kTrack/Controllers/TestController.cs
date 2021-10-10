26bade523566735319cbe4a6c362b12c951bbf95
sockpuppetcow <sockpuppetcow@gmail.com>
2021-10-10 00:15:09 -0500

    Add user manager reference to test controller + user ID test to content

--- kTrack/Controllers/TestController.cs
+++ kTrack/Controllers/TestController.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 
 namespace kTrack.Controllers
 {
     [AllowAnonymous]
     public class TestController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        
+        public TestController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             string content = User.Identity.IsAuthenticated + "\n"
                 + User.Identity.Name + "\n"
-                + User.Identity.AuthenticationType;
+                + User.Identity.AuthenticationType + "\n"
+                + _userManager.GetUserId(this.User);
             return Content(content);
         }
     }
